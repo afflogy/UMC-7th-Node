80514e98b5abd8dbb13ad276629fcbd33d9709e1
SooD <dlatnqls921@naver.com>
2024-11-11 01:09:37 +0900

    ♻️ 사용하지 않는 코드 삭제

--- src/db.config.js
+++ src/db.config.js
@@ -1,18 +1,6 @@
-import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
 
-export const pool = mysql.createPool({
-    host: process.env.DB_HOST, // mysql의 hostname
-    user: process.env.DB_USER, // user 이름
-    port: process.env.DB_PORT, // 포트 번호
-    database: process.env.DB_NAME, // 데이터베이스 이름
-    password: process.env.DB_PASSWORD, // 비밀번호
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-  });
-  
   export const prisma = new PrismaClient({ log: ["query"] });
