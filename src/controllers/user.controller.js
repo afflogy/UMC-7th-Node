e08e821e8dff696163322d5c06a0447271f600d6
SooD <dlatnqls921@naver.com>
2024-11-12 03:21:38 +0900

    [FEAT] API 응답 통일

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,12 +5,7 @@ import { userRegister } from "../services/user.service.js";
 export const handleUserRegister = async (req, res, next) => {
   console.log("회원가입을 요청했습니다!");
   console.log("body:", req.body);
-  
-  try{
-    const user = await userRegister(bodyToUser(req.body));
-    res.status(StatusCodes.OK).json({ result: user });
-  } catch(error) {
-    console.error("Error in handleUserRegister:", error);
-    res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
-  }
+
+  const user = await userRegister(bodyToUser(req.body));
+  res.status(StatusCodes.OK).success(user);
 };
